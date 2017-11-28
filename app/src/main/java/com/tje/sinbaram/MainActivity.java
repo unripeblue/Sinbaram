5e1ca07d8df48986db2464d24d388570e1b55e33
kwon <kikidy12@naver.com>
2017-11-28 10:56:47 +0900

    top버튼 생성

--- app/src/main/java/com/tje/sinbaram/MainActivity.java
+++ app/src/main/java/com/tje/sinbaram/MainActivity.java
@@ -1,14 +1,13 @@
 package com.tje.sinbaram;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 public class MainActivity extends BaseActivity {
 
@@ -17,6 +16,8 @@ public class MainActivity extends BaseActivity {
     private android.widget.LinearLayout memberLatout;
     private NavigationView naviView;
     private DrawerLayout activitymaindrawer;
+    private ImageView mainBtn;
+    private android.widget.ScrollView mainScrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +37,13 @@ public class MainActivity extends BaseActivity {
             }
         });
 
+        mainBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mainScrollView.fullScroll(ScrollView.FOCUS_UP);
+            }
+        });
+
     }
 
     @Override
@@ -46,10 +54,12 @@ public class MainActivity extends BaseActivity {
     @Override
     public void bindViews() {
         this.activitymaindrawer = (DrawerLayout) findViewById(R.id.activity_main_drawer);
-        this.naviView = (NavigationView) findViewById(R.id.naviView);
-        this.memberLatout = (LinearLayout) findViewById(R.id.memberLatout);
+        this.mainScrollView = (ScrollView) findViewById(R.id.mainScrollView);
+        this.mainBtn = (ImageView) findViewById(R.id.mainBtn);
         this.settingBtn = (ImageView) findViewById(R.id.settingBtn);
         this.menuNaviBtn = (ImageView) findViewById(R.id.menuNaviBtn);
+        this.naviView = (NavigationView) findViewById(R.id.naviView);
+        this.memberLatout = (LinearLayout) findViewById(R.id.memberLatout);
     }
 
 
