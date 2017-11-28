1636a1a6f70ab3df45b588761b7b10e7f9b979df
kwon <kikidy12@naver.com>
2017-11-28 15:06:41 +0900

    MENU 꾸미기

--- app/src/main/java/com/tje/sinbaram/MenuMemberFragment.java
+++ app/src/main/java/com/tje/sinbaram/MenuMemberFragment.java
@@ -1,13 +1,12 @@
 package com.tje.sinbaram;
 
 import android.app.Fragment;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 /**
@@ -17,12 +16,27 @@ import android.widget.TextView;
 public class MenuMemberFragment extends Fragment {
 
     private android.widget.TextView customcenterBtn;
+    private TextView textView;
+    private android.widget.LinearLayout menuViewLayout;
+    private android.widget.LinearLayout reservationLayout;
+    private android.widget.LinearLayout eventpageLayout;
+    private TextView userNameTxt;
+    private TextView userPhoneTxt;
+    private TextView userMailTxt;
+    private TextView userCouponTxt;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.frag_member_navi, container, false);
-        this.customcenterBtn = (TextView) v.findViewById(R.id.customcenterBtn);
+        this.userCouponTxt = (TextView) v.findViewById(R.id.userCouponTxt);
+        this.userMailTxt = (TextView) v.findViewById(R.id.userMailTxt);
+        this.userPhoneTxt = (TextView) v.findViewById(R.id.userPhoneTxt);
+        this.userNameTxt = (TextView) v.findViewById(R.id.userNameTxt);
+        this.eventpageLayout = (LinearLayout) v.findViewById(R.id.eventpageLayout);
+        this.reservationLayout = (LinearLayout) v.findViewById(R.id.reservationLayout);
+        this.menuViewLayout = (LinearLayout) v.findViewById(R.id.menuViewLayout);
+        this.textView = (TextView) v.findViewById(R.id.textView);
         return v;
     }
 
@@ -30,12 +44,5 @@ public class MenuMemberFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        customcenterBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:02-766-8367"));
-                startActivity(intent);
-            }
-        });
     }
 }
