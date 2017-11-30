339b16904992b0c2b0f425acc15d1f7a4c6ccb4e
kwon <kikidy12@naver.com>
2017-11-30 12:49:27 +0900

    예약신청 페이지까지 만들고 목록 페이지 만들어야할 차례

--- app/src/main/java/com/tje/sinbaram/ReservationActivity.java
+++ app/src/main/java/com/tje/sinbaram/ReservationActivity.java
@@ -1,9 +1,18 @@
 package com.tje.sinbaram;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class ReservationActivity extends BaseActivity {
+
+    private android.widget.EditText nameEdt;
+    private android.widget.EditText phoneEdt;
+    private android.widget.EditText requestEdt;
+    private android.widget.EditText reservationDateEdt;
+    private android.widget.Spinner visitCountSpinner;
+    private android.widget.Button reservationOkBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +35,11 @@ public class ReservationActivity extends BaseActivity {
 
     @Override
     public void bindViews() {
-
+        this.reservationOkBtn = (Button) findViewById(R.id.reservationOkBtn);
+        this.visitCountSpinner = (Spinner) findViewById(R.id.visitCountSpinner);
+        this.reservationDateEdt = (EditText) findViewById(R.id.reservationDateEdt);
+        this.requestEdt = (EditText) findViewById(R.id.requestEdt);
+        this.phoneEdt = (EditText) findViewById(R.id.phoneEdt);
+        this.nameEdt = (EditText) findViewById(R.id.nameEdt);
     }
 }
